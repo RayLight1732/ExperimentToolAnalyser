cc4a4b537b41da2138c0540a76f671b6a1b860fd
RayLight <arusurobo@gmail.com>
2026-01-02 21:46:34 +0900

    fix main

--- main.py
+++ main.py
@@ -1,44 +1,15 @@
 import sys
 from typing import Optional, Dict, List
-from bootstrap.wire import (
-    new_completed_subject_find_usecase,
-    new_mean_peak_fms_calculate_usecase,
-    new_peak_fms_anova_usecase,
-    new_mean_and_se_fms_usecase,
-    new_peak_fms_paired_t_test_with_holm_calculator,
-)
+from bootstrap.wire import new_completed_subject_find_usecase, new_statistics_usecase
 from bootstrap.config import load_config
 from domain.value_object.condition import Condition
 from domain.analysis.result.mean_and_se import MeanAndSE
 from domain.entity.subject import Subject
+from usecase.service.operation_registory import ValueType, CalculationType
 import matplotlib.pyplot as plt
+from domain.analysis.result.mean_and_se import MeanAndSEByCondition
 import japanize_matplotlib  # type: ignore[import-untyped]
 from bootstrap.config import Config
-
-
-def mean_peak_fms(config: Config, subjects: List[Subject]):
-    mean_peak_fms_calculate_usecase = new_mean_peak_fms_calculate_usecase(config)
-
-    for condition, value in mean_peak_fms_calculate_usecase.execute(
-        subjects
-    ).values.items():
-        print(condition, value)
-
-
-def peak_fms_anova(config: Config, subjects: List[Subject]):
-    usecase = new_peak_fms_anova_usecase(config)
-    print(usecase.execute(subjects))
-
-
-def peak_fms_paired_t_and_holm(config: Config, subjects: List[Subject]):
-    usecase = new_peak_fms_paired_t_test_with_holm_calculator(config)
-    result = usecase.execute(subjects)
-    for condition_tuple, value in result.values.items():
-        print(
-            condition_tuple[0].mode.display_name,
-            condition_tuple[1].mode.display_name,
-            value,
-        )
 
 
 def plot_mean_and_se(
@@ -85,18 +56,19 @@ def plot_mean_and_se(
     plt.show()
 
 
-def mean_and_se_peak_fms(config: Config, subjects: List[Subject]):
-    usecase = new_mean_and_se_fms_usecase(config)
-    plot_mean_and_se(usecase.execute(subjects).values)
-
-
 def main(config_path: Optional[str] = None) -> None:
     config = load_config(config_path)
     usecase = new_completed_subject_find_usecase(config)
     subjects = usecase.execute()
     for subject in subjects:
         print(subject.data.name)
-    peak_fms_paired_t_and_holm(config, subjects)
+    statistics_usecase = new_statistics_usecase(config)
+    result: MeanAndSEByCondition = statistics_usecase.execute(
+        subjects,
+        value_type=ValueType.FMS,
+        calculation_type=CalculationType.MEAN_AND_SE,
+    )
+    plot_mean_and_se(result.values, title="FMS Mean and SE")
 
 
 if __name__ == "__main__":
